c99442d8e58470110d154a77a5b240bb91cefbd0
Gabriel Prado <guardavege@gmail.com>
2023-12-24 15:54:39 -0300

    Crud Criado

--- Api-Library/server.js
+++ Api-Library/server.js
@@ -2,6 +2,9 @@ import app from "./src/app-express.js";
 
 const PORT = 3000;
 
+
+
+
 app.listen(PORT, () =>
 {
     console.log("Server Criado");
--- Api-Library/src/app-express.js
+++ Api-Library/src/app-express.js
@@ -1,10 +1,71 @@
 import Express  from "express";
 
 const app = Express();
+app.use(Express.json());
+
+const Livros = 
+[
+    {
+        "Id" : 1,
+        "Livro" : "Harry Poter"
+    },
+    {
+        "Id" : 2,
+        "Livro" : "Pc Gamer"
+    }
+
+];
+
+function GetId(id)
+{
+    return Livros.findIndex(Livros => 
+        {
+            return Livros.Id === Number(id);
+        });
+}
+
+app.get("/livros" ,(req,res) =>
+{
+    res.status(200).json(Livros);
+});
 
 app.get("/", (req,res) =>
 {
     res.status(200).send("Raiz do Projeto");
+});
+
+app.post("/livros",(req,res) =>
+{
+    Livros.push(req.body);
+    res.status(201).send("Livro Cadastrado");
+});
+
+app.get("/livros/:id" ,(req,res) => 
+{
+    const id = GetId(req.params.id);
+   
+        res.status(200).json(Livros[id]);
+  
+});
+
+app.put("/livros/:id", (req,res) =>
+{
+    const id = GetId(req.params.id);
+    if(req.body.Id != Livros[id].Id)
+    {
+        res.send("Não é possivel Atualizar o Id")
+    }else
+    {
+    Livros[id] = req.body
+    res.status(201).send("Livro Atualizado");
+    }
+});
+
+app.delete("/livros/:id" ,(req,res) => 
+{
+    const id = GetId(req.params.id);
+    Livros.splice(id,1);
+    res.status(201).send("Livro Excluido");
 });
 
 export default app;
